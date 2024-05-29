8d94d16c1aa8f8bc5060e9a8c2c83484a8bdf30f
Drekoogfx <ythugocrack@gmail.com>
2024-05-29 16:52:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,46 +4,52 @@ const app = express();
 
 app.listen(3000, () => {
   console.log('Project is running!');
-})
+});
 
 app.get('/', (req, res) => {
   res.send('Hello world!');
-})
+});
 
 const Discord = require("discord.js");
-const client = new Discord.Client({intents: ["GUILDS", "GUILD_MESSAGES"]});
-
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
 
 client.on("messageCreate", message => {
-  if(message.content === "ping") {
-    message.channel.send("pong")
+  if (message.content === "ping") {
+    message.channel.send("pong");
   }
-  if(message.content === "!paypal") {
-    let paypal = new Discord.MessageEmbed()
-    .setTitle("Pago mediante paypal")
-    .setDescription(`**esp :flag_es: **
-                    A la hora de hacer el pago **aceptas** los <#1060201345073815612>
-                    Para realizar el pago debe enviar el pago por amigos y familiares y sin concepto, cuando lo haya realizado deberá mandar captura
+
+  if (message.content === "!paypal") {
+    // Verificar si el usuario tiene permisos de administrador
+    if (message.member.permissions.has("ADMINISTRATOR")) {
+      let paypal = new Discord.MessageEmbed()
+        .setTitle("Pago mediante PayPal")
+        .setDescription(`**ESP :flag_es:**
+                    A la hora de hacer el pago **aceptas** los <#1060201345073815612>.
+                    Para realizar el pago debe enviar el pago por amigos y familiares y sin concepto, cuando lo haya realizado deberá mandar captura.
                     **(En caso de que algo se incumpla no se te dará el producto)**
 
-                   ** eng :flag_gb:**
-                    By making the payment, you accept the <#1060201345073815612> .
+                   **ENG :flag_gb:**
+                    By making the payment, you accept the <#1060201345073815612>.
                     To make the payment, you must send it as friends and family without any description. Once you have done this, you must send a screenshot.
                     **(In case any of these conditions are not met, the product will not be given to you)**
 
+                    https://www.paypal.com/paypalme/GFXDREKO`)
+        .setFooter("No se admiten devoluciones")
+        .setColor("#FFFFFF")
+        .setThumbnail("https://pngimg.com/uploads/paypal/paypal_PNG7.png");
 
-                    https://www.paypal.com/paypalme/GFXDREKO `)
-    .setFooter("No se admiten devoluciones")
-    .setColor("#FFFFFF")
-    .setThumbnail("https://pngimg.com/uploads/paypal/paypal_PNG7.png")
-
-
-    message.channel.send({embeds:[paypal]})
+      message.channel.send({ embeds: [paypal] });
+    } else {
+      message.reply("No tienes permisos para usar este comando.");
+    }
   }
-  if(message.content === "!bizum") {
-    let bizum = new Discord.MessageEmbed()
-    .setTitle("Pago mediante Bizum")
-    .setDescription(`**ESP** :flag_es:
+
+  if (message.content === "!bizum") {
+    // Verificar si el usuario tiene permisos de administrador
+    if (message.member.permissions.has("ADMINISTRATOR")) {
+      let bizum = new Discord.MessageEmbed()
+        .setTitle("Pago mediante Bizum")
+        .setDescription(`**ESP** :flag_es:
 
     > - Cantidad mínima: **25€ eur**
     > 
@@ -59,13 +65,16 @@ client.on("messageCreate", message => {
     > 
    > - Send a **screenshot** when making the payment
     `)
-    .setFooter("No se admiten devoluciones")
-    .setColor("#FFFFFF")
-    .setThumbnail("https://upload.wikimedia.org/wikipedia/commons/2/24/Bizum.png")
+        .setFooter("No se admiten devoluciones")
+        .setColor("#FFFFFF")
+        .setThumbnail("https://upload.wikimedia.org/wikipedia/commons/2/24/Bizum.png");
 
-
-    message.channel.send({embeds:[bizum]})
+      message.channel.send({ embeds: [bizum] });
+    } else {
+      message.reply("No tienes permisos para usar este comando.");
+    }
   }
-})
+});
 
 client.login(process.env.token);
+
